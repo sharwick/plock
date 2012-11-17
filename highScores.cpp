e1a582f891b67bede5859f5f8ba93d371e4b8265
Devin Rusnak <devin_rusnak@devin-HP-Pavilion-g6-Notebook.(none)>
2012-11-17 17:51:35 -0600

    Added some formated comments.

--- highScores.cpp
+++ highScores.cpp
@@ -1,6 +1,21 @@
+/**@author Devin Rusnak
+ * @copyright Blockstar 2012
+ * @class HighScores highScores.h "highScores.h"
+ * @brief This is a class used to read & write high scores from the game to a file for record.
+ * @bug High Scores do not transfer to mainwindow_UI correctly.
+ *
+ * The high scores from the game are stored in a .txt file that is read and wrote to.
+ * The top ten scores from each mode is read & wrote to in a specific order to maintain simplicity
+ * in the file format.
+ * The high scores from each game mode are held in seperate arrays of integers.
+ * These scores are used by the score# labels in mainwindow_UI to display the scores to the user.
+*/
 #include "highScores.h"
 
-
+/**
+ * @brief HighScores::HighScores
+ *
+ */
 HighScores::HighScores()
 {
     theFile = new QFile("highscores.txt");
@@ -12,10 +27,17 @@ HighScores::HighScores()
     }
 }
 
+/**
+ * @brief HighScores::~HighScores
+ */
 HighScores::~HighScores(){
-
+    delete theFile;
+    delete inputStream;
 }
 
+/**
+ * @brief HighScores::readInHighScores
+ */
 void HighScores::readInHighScores(){
 
     int index = 0;
@@ -31,10 +53,18 @@ void HighScores::readInHighScores(){
     }
 }
 
+/**
+ * @brief HighScores::writeHighScores
+ */
 void HighScores::writeHighScores(){
 
 }
 
+/**
+ * @brief HighScores::getScore
+ * @param s Index of the score wanted.
+ * @return Returns score at the index requested.
+ */
 int HighScores::getScore(int s){
     if(highScoresArray[s] == NULL)
         return 0;
@@ -43,6 +73,10 @@ int HighScores::getScore(int s){
         return highScoresArray[s];
 }
 
+/**
+ * @brief HighScores::addHighScore
+ * @param score Score that is sorted into the proper array.
+ */
 void HighScores::addHighScore(int score){
 
 }
--- myRectItem.cpp
+++ myRectItem.cpp
@@ -1,23 +1,51 @@
+/** @author Devin Rusnak
+ *  @author Daniel Keasler
+ *  @copyright Blockstar 2012
+ *  @class myRectItem myRectItem.h "myRectItem.h"
+ *  @extends QGraphicsRectItem
+ *  @brief Custom QGraphicsRectItem
+ *
+ *  A custom QGraphicsRectItem that has specific methods to handle the mouse clicks and bomb images.
+ */
+
 #include "myRectItem.h"
 #include "mainwindow_UI.h"
 
+/**
+ * @brief myRectItem::myRectItem
+ */
 myRectItem::myRectItem()
 {
     bombPtr = new QGraphicsEllipseItem();
 }
 
+/**
+ * @brief myRectItem::mousePressEvent
+ */
 void myRectItem::mousePressEvent(QGraphicsSceneMouseEvent *){
     this->grabMouse();
 
 }
+
+/**
+ * @brief myRectItem::mouseMoveEvent
+ */
 void myRectItem::mouseMoveEvent(QGraphicsSceneMouseEvent *){
     this->grabMouse();
 }
 
+/**
+ * @brief myRectItem::setEllipse
+ * @param paramBomb
+ */
 void myRectItem::setEllipse(QGraphicsEllipseItem *paramBomb){
     bombPtr = paramBomb;
 }
 
+/**
+ * @brief myRectItem::removeGraphObject
+ * @param theScene
+ */
 void myRectItem::removeGraphObject(QGraphicsScene* theScene){
     if(bombPtr != 0){
         theScene->removeItem(bombPtr);
